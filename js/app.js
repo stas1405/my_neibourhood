aeca68ff423c1d858d970768ef8dbac23dbc7780
stas1405 <stas1405@yandex.ru>
2016-01-28 17:12:19 -0800

    fix: issue with bounds and async script

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-  var map;
-  var infowindow;
+var map;
+var infowindow;
 
 var MarkerModel = function(marker, name, contact, position) {
   this.marker = marker;
@@ -10,7 +10,7 @@ var MarkerModel = function(marker, name, contact, position) {
   this.position = position;
 };
 
-function MapViewModel() {
+var MapViewModel = function() {
   var self = this;
   var venueMarkers = [];
   self.listBoolean = ko.observable(true);
@@ -19,7 +19,7 @@ function MapViewModel() {
   self.searchedList = ko.observableArray(self.locationsList()); //List of locations for search 
   var foursquareBaseUrl = "https://api.foursquare.com/v2/venues/explore?ll=49.246292,-123.116226&&limit=20&section=topPicks&day=any&time=any&locale=en&oauth_token=GZVQCNRCS5141KWELGOWRD35HCOD1NM03PDRUXE4PHZEFH51&v=20160106";
 
-  self.listToggle = function () {
+  self.listToggle = function() {
     if (self.listBoolean() === true) {
       self.listBoolean(false);
     } else {
@@ -27,33 +27,34 @@ function MapViewModel() {
     }
   };
 
-  self.markerClick = function () {
-      self.listBoolean(false); 
+  self.markerClick = function() {
+    self.listBoolean(false);
   };
-  self.closeclick = function(){
-      self.listBoolean(true); 
+  self.closeclick = function() {
+    self.listBoolean(true);
   };
 
-    // Getting list of location from Foursquare
+  // Getting list of location from Foursquare
   $.getJSON(foursquareBaseUrl, function(data) {
     self.locationsList(data.response.groups[0].items);
 
     //Function to sort list of location.
-    self.sortLocations = function () {
-      self.locationsList.sort(function(left, right) { 
-          return left.venue.name == right.venue.name ? 0 : (left.venue.name < right.venue.name ? -1 : 1);
+    self.sortLocations = function() {
+      self.locationsList.sort(function(left, right) {
+        return left.venue.name == right.venue.name ? 0 : (left.venue.name < right.venue.name ? -1 : 1);
       });
     };
     self.sortLocations();
     self.searchedList(self.locationsList());
+    self.bounds = new google.maps.LatLngBounds();
 
     //Creating markers to pop up the map
     self.locLen = self.locationsList().length;
-    for (var l = 0; l < self.locLen;  l++) {
+    for (var l = 0; l < self.locLen; l++) {
       createMarkers(self.locationsList()[l]);
     }
+    map.fitBounds(self.bounds);
   }).fail(function(jqXHR, status, error) {
-    console.log(status);
     if (status == 'error') {
       alert("Forsquare API is not reachable. Try to refresh this page later");
     }
@@ -81,11 +82,10 @@ function MapViewModel() {
 
   self.updateList = function(data) {
     var venueName = data.venue.name.toLowerCase();
-    var venueMarkersLen  = venueMarkers.length;
+    var venueMarkersLen = venueMarkers.length;
     for (var i = 0; i < venueMarkersLen; i++) {
       if (venueMarkers[i].name === venueName) {
         google.maps.event.trigger(venueMarkers[i].marker, 'click');
-
         map.panTo(venueMarkers[i].position);
       }
     }
@@ -114,17 +114,14 @@ function MapViewModel() {
     var lat = placeData.venue.location.lat;
     var lng = placeData.venue.location.lng;
     var name = placeData.venue.name;
-    var category = placeData.venue.categories[0].name;
     var position = new google.maps.LatLng(lat, lng);
     var address = placeData.venue.location.formattedAddress;
     var contact = placeData.venue.contact.formattedPhone;
-    var describtion = placeData.tips[0].text;
-    window.mapBounds = new google.maps.LatLngBounds();
-    var bounds = window.mapBounds; // current boundaries of the map window
 
-    bounds.extend(new google.maps.LatLng(lat, lng));
-    map.fitBounds(bounds);
-    map.setCenter(bounds.getCenter());
+    self.bounds.extend(new google.maps.LatLng(lat, lng));
+    map.fitBounds(self.bounds);
+    map.setCenter(self.bounds.getCenter());
+
     var icon = {
       url: "img/pin1.png", // url
       scaledSize: new google.maps.Size(30, 30), // scaled size
@@ -139,10 +136,11 @@ function MapViewModel() {
       animation: google.maps.Animation.DROP,
       icon: icon
     });
+
     marker.addListener('click', toggleBounce);
 
     google.maps.event.addListener(marker, 'click', function() {
-      self.markerClick(); 
+      self.markerClick();
       infowindow.setContent(contentWindow);
       infowindow.open(map, this);
       map.panTo(position);
@@ -160,17 +158,21 @@ function MapViewModel() {
     }
 
     venueMarkers.push(new MarkerModel(marker, name.toLowerCase(), contact, position));
+
     // DOM element for infowindow content
     var contentWindow = '<div class="markerInfo"><p id="locationName">' + name +
       '</p><p id="locationAddress">' + contact +
       '</p><p id="locationDescription">' + address;
   }
-}
+};
 
+var myModel = {
+  viewModel: new MapViewModel()
+};
 
 function initializeMap() {
   var mapOptions = {
-    maxZoom: 13,
+    zoom:19,
     scrollwheel: true,
     disableDefaultUI: false
   };
@@ -178,23 +180,22 @@ function initializeMap() {
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
   infowindow = new google.maps.InfoWindow();
 
-      google.maps.event.addListener(infowindow, 'closeclick', function() {
-        myModel.viewModel.listToggle();
-    });
+  google.maps.event.addListener(infowindow, 'closeclick', function() {
+    myModel.viewModel.listToggle();
+  });
 }
+
+var googleSuccess = function() {
+
+  initializeMap();
+  ko.applyBindings(myModel.viewModel);
+};
+
+window.addEventListener('resize', function(e) {
+
+  map.fitBounds(self.bounds);
+});
 
 function handleError() {
   alert("There is a problem loading Google Maps API. Check your reference.");
 }
-
-window.addEventListener('load', initializeMap);
-
-window.addEventListener('resize', function(e) {
-
-  map.fitBounds(mapBounds);
-});
-
-
-
-var myModel = { viewModel: new MapViewModel() };
-ko.applyBindings(myModel.viewModel);
